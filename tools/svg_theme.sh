ad02eef534fd0b74387622e2d5e22b1a977403a1
osmpaws <speirs@email.cz>
2019-05-25 07:31:49 +0200

    This is automatic commit of release r00046 ( build b00379 )

--- tools/svg_theme.sh
+++ tools/svg_theme.sh
@@ -127,7 +127,7 @@ echo ""
 echo '/<\s*symbol / s/rotate="[^"]*" //g
 /<\s*symbol / s/repeat-start="[^"]*" //g
 /<\s*symbol / s/repeat-gap="[^"]*" //g
-/<\s*symbol / s/\s*\/>/ priority="2" \/>/g' >> $sedscript
+/<\s*symbol / {/priotity="[0-9]+"/ s/\s*\/>/ priority="2" \/>/g}' >> $sedscript
 sed -i -f $sedscript $targetdir/$themename/$newthemename.xml
 
 if ! xmllint --noout "$targetdir/$themename/$newthemename.xml" ; then
