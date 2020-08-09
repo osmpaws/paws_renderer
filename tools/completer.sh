d6c6b511caf29592eee6bb517571c80f0d2f36d5
osmpaws <speirs@email.cz>
2020-08-09 09:27:35 +0200

    This is automatic commit of release r00064 ( build b00466 )

--- tools/completer.sh
+++ tools/completer.sh
@@ -18,7 +18,7 @@ imgfiles=`grep 'src=' "$root/$1/$1.xml" | sed 's/.*src=file:"\(.*\)".*/\1/g' | s
 
 for filepath in $imgfiles;
 do
-	pattern=`echo "$filepath" | rev | cut -d. -f2- | rev | sed -e 's/[^a-z/]/./g'`
+	pattern=`echo "$filepath" | rev | cut -d. -f2- | rev | LC_ALL=C sed -e 's/[^a-z/]/./g'`
 	if ! grep -q -m1 "$pattern" $refreshlist ; then
 		continue
 	else
