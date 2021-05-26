6db1e49f03d78256b800807e5097b98177db4ee0
Jan Skala <speirs@email.cz>
2021-05-26 20:22:27 +0200

    Cumulative update after system refresh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-aptitude install python3 meld inkscape python3-flask python-yaml
+aptitude install python3 meld inkscape python3-flask python-yaml libxml2-utils git
 
 cp system/osm-themes-webserver.service /etc/systemd/system
 
